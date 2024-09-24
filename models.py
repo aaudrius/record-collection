c770465451589b7c1a5c2be5e37f46fe0069dd86
Audrius Anusauskas <audrius.anusauskas@vinted.com>
2024-09-24 11:07:01 +0300

    update models.py

--- models.py
+++ models.py
@@ -35,5 +35,5 @@ Users.followees = db.relationship('Users',
                                lazy='dynamic')
 def init_db(app):
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
